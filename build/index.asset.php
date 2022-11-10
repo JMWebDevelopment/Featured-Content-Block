2e716b51d932f05b496a2ee808390850829d39df
Jacob Martella <me@jacobmartella.com>
2022-11-09 20:18:41 -0600

    Initial commit.
    Addresses #1

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '079d5e9d61880e4a2aa8');
---
+++ featured-post-block.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name:       Featured Post Block
+ * Requires at least: 5.9
+ * Requires PHP:      7.0
+ * Version:           1.0.0
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       featured-post-block
+ *
+ * @package           featured-post-block
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function featured_post_block_featured_post_block_block_init() {
+	register_block_type(
+		__DIR__ . '/build',
+		array(
+			'render_callback' => 'featured_post_block_featured_post_block_render_callback',
+		)
+	);
+}
+add_action( 'init', 'featured_post_block_featured_post_block_block_init' );
+
+/**
+ * Render callback function.
+ *
+ * @param array    $attributes The block attributes.
+ * @param string   $content    The block content.
+ * @param WP_Block $block      Block instance.
+ *
+ * @return string The rendered output.
+ */
+function featured_post_block_featured_post_block_render_callback( $attributes, $content, $block ) {
+	ob_start();
+	require plugin_dir_path( __FILE__ ) . 'build/template.php';
+	return ob_get_clean();
+}
